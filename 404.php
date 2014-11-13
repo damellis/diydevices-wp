b9a36a2629ba74d4231b18b6bd43828723f89686
David A. Mellis <dam@mellis.org>
2014-11-13 11:52:33 -0500

    Add 404 not found page template.

---
+++ 404.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+<div id="menu">
+</div>
+
+<div class="main">
+<h1>Page Not Found</h1>
+<p>
+Sorry, we couldn't find the page you were looking for.
+</p>
+<?php
+if ( have_posts() ) :
+	while ( have_posts() ) : the_post();
+		get_template_part( 'content', get_post_format() );
+	endwhile;
+
+else :
+	get_template_part( 'content', 'none' );
+endif;
+?>
+</div>
+
+<?php
+get_sidebar();
+get_footer();
