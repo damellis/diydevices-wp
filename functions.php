30bbac2ac915f8f98f6b14982947891ad88bfe60
David Mellis <dam@mellis.org>
2014-12-05 16:14:19 -0600

    Add three-quarter size images.

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ add_filter( 'pre_option_link_manager_enabled', '__return_true' );
 add_image_size( 'quarter-width', 198, 9999 );
 add_image_size( 'third-width', 270, 9999 );
 add_image_size( 'half-width', 414, 9999 );
+add_image_size( 'three-quarter-width', 630, 9999 );
 add_image_size( 'full-width', 846, 9999 );
 
 add_filter( 'image_size_names_choose', 'my_custom_sizes' );
@@ -13,6 +14,7 @@ function my_custom_sizes( $sizes ) {
         'quarter-width' => __('Quarter Width'),
         'third-width' => __('Third Width'),
         'half-width' => __('Half Width'),
+        'three-quarter-width' => __('Three Quarter Width'),
         'full-width' => __('Full Width'),
     ) );
 }
